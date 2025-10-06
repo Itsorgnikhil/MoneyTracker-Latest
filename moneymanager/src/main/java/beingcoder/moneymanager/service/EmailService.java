571d1e29de91eb13b7b90dc96a2420933de8e6f7
Itsorgnikhil <150001329+Itsorgnikhil@users.noreply.github.com>
2025-10-06 10:49:08 +0530

    Update EmailService.java

--- moneymanager/src/main/java/beingcoder/moneymanager/service/EmailService.java
+++ moneymanager/src/main/java/beingcoder/moneymanager/service/EmailService.java
@@ -1,9 +1,9 @@
 package beingcoder.moneymanager.service;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,19 +11,20 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender mailSender;
-    @Value("${spring.mail.properties.mail.smtp.from}")
-    private String fromEmail;
-
 
     public void sendEmail(String to, String subject, String body) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(fromEmail);
-            message.setTo(to);
-            message.setText(body);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true); // true = HTML
+
             mailSender.send(message);
+            System.out.println("Activation email sent to: " + to);
         } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+            System.err.println("Failed to send email: " + e.getMessage());
         }
     }
 }
